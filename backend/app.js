576023ee7e694d77cf3c99cf0bbe4366fc1dbaf6
Krzychu <krzysztof.dymicki.bp@gmail.com>
2020-05-22 17:11:00 +0200

    Connected to mongoDB

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,16 @@
+const config = require('./utils/config')
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
+
+console.log('Connecting to mongoDB')
+
+mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false})
+	.then(() => {
+		console.log('connected to mongoDB')
+	})
+	.catch((error) => {
+		console.log('error with connecting to mongoDB', error.message)
+	})
 
 module.exports = app
